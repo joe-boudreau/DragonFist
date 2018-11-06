9c9ee059ba31c2ef49cdf55ae350aee4d068508b
Andrew Ferrazzutti <andrew.ferrazzutti@alum.utoronto.ca>
2018-11-06 03:52:42 -0500

    Plot & save filtered images; fix filter usage
    Add options to plot a chosen number of filtered vs unfiltered images,
    and to save them to disk in an auto-named directory.
    
    Fix improper usage of filter functions by ImageDataGenerator.

--- src/dragonfist/transform_feed_model.py
+++ src/dragonfist/transform_feed_model.py
@@ -8,40 +8,49 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.activations import relu, softmax
 
 from matplotlib import pyplot as plt
-from matplotlib import cm
 
 import functools
+import os
 
 import transformations
 
-# TODO Can probably use any image dataset with this. If so, make it variable
-(dataset, num_classes) = (keras.datasets.cifar10, 10)
+# Global settings
+epochs = 10
+# NOTE: By default, datagen.flow uses a batch size of 32.
+#       Tweak to find best tradeoff between runtime & memory.
+#       (Lower is slower, higher is more memory)
+generator_batch_size=32
+# NOTE: Must pick good number of workers knowing that there will
+#       be filter-level parallelism too.
+generator_workers=1
 
+# Dataset-specific settings
+# TODO If the model is good for any image classification,
+#      set these in a function with custom parameters
+#      instead of hardcoding them.
+dataset = keras.datasets.cifar10
+num_classes = 10
+rescale = 1/255.0
+
+# Prepare data
 (train_images, train_labels), (test_images, test_labels) = dataset.load_data()
+train_images = train_images * rescale
+test_images = test_images * rescale
 input_shape = train_images.shape[1:]
-
-# FOR TESTING ONLY: reduce size of training & test sets
-#train_images = train_images[:100]
-#train_labels = train_labels[:100]
-#test_images = test_images[:100]
-#test_labels = test_labels[:100]
 
 # Use one-hot encoding for labels
 train_labels = keras.utils.to_categorical(train_labels, num_classes)
 test_labels  = keras.utils.to_categorical(test_labels, num_classes)
 
-# https://cs231n.github.io/neural-networks-2/
-# https://ieeexplore-ieee-org.myaccess.library.utoronto.ca/stamp/stamp.jsp?tp=&arnumber=7808140
-# preprocessing for images:
-# - zero centering / mean subtraction (either all at once, or for each color channel)
-# - normalization / scaling (not strictly necessary for images)
-# - PCA/ZCA, Whitening (course claims this isn't used with CNNs, but paper says it's the most important one to use!)
-# Keras has all of these already, in keras.preprocessing.image
 
-def main(image_filter=transformations.identity, filter_params={}, preprocess=False, preprocess_params={}):
+def main(image_filter=transformations.identity, filter_params={}, preprocess=True, preprocess_params={},
+         plot_filtered_images=5, save_location='genimage'):
+
     # CNN for learning CIFAR-10, based on this example:
     # https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
     # TODO Need to do some research to find a good image classifier CNN, then implement it.
+    #      Don't forget about regularizers!
+    #      Also consider constraints and initializers.
     model = Sequential([
         # Conv2D(filters, kernel_size, ...)
         # Dropout(rate, ...)
@@ -62,50 +71,78 @@ def main(image_filter=transformations.identity, filter_params={}, preprocess=Fal
         Dense(num_classes, activation=softmax)
     ])
 
-    # Want to use gradient descent, but example used RMSprop.
-    # So, using the SGD optimzer from this example:
-    # https://keras.io/getting-started/sequential-model-guide/#vgg-like-convnet
+    # TODO Cross-validate to find best hyperparameters
     model.compile(optimizer=keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True),
                   loss=keras.losses.categorical_crossentropy,
                   metrics=[keras.metrics.categorical_accuracy])
 
+
+    filter_name = image_filter.__name__
     if filter_params != {}:
         image_filter = functools.partial(image_filter, **filter_params)
 
+    if plot_filtered_images > 0:
+        if save_location != None and save_location != '':
+            save_location += '/' + filter_name + get_suffix_from_params(filter_params)
+            os.makedirs(save_location, exist_ok=True)
+
+        # NOTE: Use a different generator here to show only the effects of the filter,
+        #       and not of any other preprocessing step.
+        filtergen = ImageDataGenerator(preprocessing_function=image_filter)
+        image_generator = filtergen.flow(train_images, train_labels, batch_size=1, shuffle=False, save_to_dir=save_location)
+
+        i = 0
+        fig, axs = plt.subplots(2, plot_filtered_images)
+        for batch_images, batch_labels in image_generator:
+            ax = axs[0,i]
+            ax.imshow(train_images[i])
+            ax.set_axis_off()
+            ax = axs[1,i]
+            ax.imshow(batch_images[0])
+            ax.set_axis_off()
+
+            i = i+1
+            if i == plot_filtered_images:
+                plt.show()
+                break
+
+
     if preprocess:
-        datagen = ImageDataGenerator(rescale=1/255.0, preprocessing_function=image_filter, **preprocess_params)
+        datagen = ImageDataGenerator(preprocessing_function=image_filter, **preprocess_params)
 
-        # Compute quantities required for feature-wise normalization
-        # (std, mean, and principal components if ZCA whitening is applied).
-        # TODO uncomment this when any of the above are applied
-        #datagen.fit(train_images)
+        # Always run this in case one of the preprocess_params enables feature-wise normalization.
+        datagen.fit(train_images)
 
-        # Interesting methods:
-        # datagen.apply_transform(x, transform_parameters)
-        # datagen.random_transform(x, seed)
-        # datagen.standardize(x)
+        # TODO Don't use test set as validation set
+        train_generator      = datagen.flow(train_images, train_labels, batch_size=generator_batch_size)
+        validation_generator = datagen.flow(test_images,  test_labels,  batch_size=generator_batch_size)
 
-        # NOTE: To save the preprocessed images to disk, pass save_to_dir='genimage'.
-        # WARNING!!! It will save hundreds of images, so it might kill your hard drive.
-        # Lower the training/test set size and the number of epochs before using save_to_dir.
-        # TODO Add testing code to iterate over datagen.flow and pick some images to plot
-        #      with matplotlib or to save to disk, instead of saving all of them.
-        #
-        # NOTE: By default, datagen.flow uses a batch size of 32.
-        #       Using 1 would be nice but it takes too long, and
-        #       using the entire set as a batch takes too much memory.
-        batch_size=32
-        model.fit_generator(datagen.flow(train_images, train_labels, batch_size=batch_size),
-                            validation_data=datagen.flow(test_images, test_labels, batch_size=batch_size),
-                            epochs=10,
-                            workers=4)
+        model.fit_generator(train_generator,
+                            validation_data=validation_generator,
+                            epochs=epochs,
+                            workers=generator_workers)
+
+        test_loss, test_acc = model.evaluate_generator(datagen.flow(test_images, test_labels, batch_size=generator_batch_size))
+        print('Test accuracy:', test_acc)
 
     else:
-        filtered_train_images = image_filter(train_images/255.0, **filter_params)
-        filtered_test_images  = image_filter(test_images /255.0, **filter_params)
-        model.fit(filtered_train_images, train_labels, epochs=10,
+        filtered_train_images = transformations.multi(image_filter, train_images)
+        filtered_test_images  = transformations.multi(image_filter, test_images)
+        # TODO Don't use test set as validation set
+        model.fit(filtered_train_images, train_labels, epochs=epochs,
                   validation_data=(filtered_test_images, test_labels))
 
-main()
-main(transformations.edge_detection)
-main(transformations.gaussian, filter_params={'sigma':0.5})
+        test_loss, test_acc = model.evaluate(filtered_test_images, test_labels)
+        print('Test accuracy:', test_acc)
+
+
+def get_suffix_from_params(param_dict):
+    suffix = ''
+    for key, value in param_dict.items():
+        suffix += '-{}_{}'.format(key, value)
+    return suffix
+
+
+main(plot_filtered_images=0, preprocess=False)
+main(transformations.edge_detection_3d)
+main(transformations.gaussian, filter_params={'sigma':2})
--- src/dragonfist/transformations.py
+++ src/dragonfist/transformations.py
@@ -1,65 +1,75 @@
 import skimage
 from scipy import ndimage
-from skimage import filters
+from skimage import filters, color
 import numpy as np
 
-"""This module contains the image transformations. Each function in this module should accept an N-dimensional numpy 
-array and return an N-dimensional numpy array. Some may accept additional hyperparameters for tuning the filters"""
+"""This module contains the image transformations. Each function in this module should accept and return a
+Numpy array representing an image. Some may accept additional hyperparameters for tuning the filters"""
+
+def multi(filter, images):
+    return np.array([filter(i) for i in images])
 
 
-def identity(images):
-    return images
+def compat3d(filter, image, *args):
+    return color.gray2rgb(filter(color.rgb2gray(image), *args))
 
 
-def edge_detection(images):
-    return np.array([filters.sobel(i) for i in images])
+def identity(image):
+    return image
 
 
-def gabor_real(images, frequency=0.8):
+def edge_detection(image):
+    return filters.sobel(image)
+
+def edge_detection_3d(image):
+    return compat3d(edge_detection, image)
+
+
+def gabor_real(image, frequency=0.8):
     """Seems like increase in frequency correlates with model accuracy. training on images filtered with f = 0.5 results
         in accuracy around 50%. If f = 0.8 the accuracy jumps up to 80%"""
-    return np.array([filters.gabor(i, frequency)[0] for i in images])
+    return filters.gabor(image, frequency)[0]
+
+def gabor_real_3d(image, frequency=0.8):
+    return compat3d(gabor_real, image, frequency)
 
 
-def gabor_imaginary(images, frequency=0.5):
-    return np.array([filters.gabor(i, frequency)[1] for i in images])
+def gabor_imaginary(image, frequency=0.5):
+    return filters.gabor(image, frequency)[1]
+
+def gabor_imaginary_3d(image, frequency=0.5):
+    return compat3d(gabor_imaginary, image, frequency)
 
 
-def gaussian(images, sigma=1):
+def gaussian(image, sigma=1):
     """Even with sigma = 2 stanard deviations it was still able to achieve close to 80% accuracy on the MNIST clothes
         classifier. I think in general we want to tune the filters to distort the image as much as possible while still
         achieving at least ~80% accuracy on the training/test data. More distortion = higher robustness?"""
-    return np.array([filters.gaussian(i, sigma) for i in images])
+    return filters.gaussian(image, sigma)
 
 
-def sharpen(images):
+# TODO It looks like this actually blurs images
+def sharpen(image):
     """This filter doesn't really work for low resolution images like the MNIST fashion dataset (28x28px). Might work if
     we try higher resolution images"""
-    def sharpenImg(i):
-        blurred_img = ndimage.gaussian_filter(i, 1)
+    blurred_img = ndimage.gaussian_filter(image, 1)
 
-        filter_blurred_img = ndimage.gaussian_filter(blurred_img, 0.3)
+    filter_blurred_img = ndimage.gaussian_filter(blurred_img, 0.3)
 
-        alpha = 30
-        return blurred_img + alpha * (blurred_img - filter_blurred_img)
-
-    return np.array([sharpenImg(i) for i in images])
+    alpha = 30
+    return blurred_img + alpha * (blurred_img - filter_blurred_img)
 
 
-def increase_contrast(images):
+def increase_contrast(image):
     """This doesn't work for images which already use the maximum range of the pixels's dtype. Need to use
     the increase_contrast_saturated function below in that case"""
-    return np.array([skimage.exposure.rescale_intensity(i) for i in images])
+    return skimage.exposure.rescale_intensity(image)
 
 
-def increase_contrast_saturated(images):
+def increase_contrast_saturated(image):
     """This function essentially takes the top and bottom 'p' percent of pixel values in the image
     and rescales them to the max and min value allowed by the dtype. Seems to still produce accurate classifications
     even on models trained with no filter applied"""
-    def inc(i):
-        p = 5
-        (v_min, v_max) = np.percentile(i, (p, 100-p))
-        return skimage.exposure.rescale_intensity(i, in_range=(v_min, v_max))
-
-    return np.array([inc(i) for i in images])
-
+    p = 5
+    (v_min, v_max) = np.percentile(image, (p, 100-p))
+    return skimage.exposure.rescale_intensity(image, in_range=(v_min, v_max))
