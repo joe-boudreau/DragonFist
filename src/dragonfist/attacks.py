0ddd5aebe687f0fe29a1bfb95218a688caba9882
joeflow <thejoeflow@gmail.com>
2018-11-25 18:52:59 -0500

    Added a blackbox attack in attacks.py
    Accepts a trained model and a dataset. The dataset needs to be of type float32 for some reason, so ive added an additional param to Dataset objects to specify the dtype

--- src/dragonfist/attacks.py
+++ src/dragonfist/attacks.py
@@ -1,13 +1,34 @@
-import keras
-from keras.preprocessing.image import ImageDataGenerator
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import functools
+import logging
 
 import cleverhans
-from cleverhans.attacks import FastGradientMethod, SaliencyMapMethod
-from cleverhans.utils_keras import KerasModelWrapper
-
-from matplotlib import pyplot as plt
-from matplotlib import cm
+import keras
 import numpy as np
+import tensorflow as tf
+from cleverhans.attacks import FastGradientMethod
+from cleverhans.attacks import SaliencyMapMethod
+from cleverhans.attacks_tf import jacobian_graph, jacobian_augmentation
+from cleverhans.evaluation import batch_eval
+from cleverhans.initializers import HeReLuNormalInitializer
+from cleverhans.loss import CrossEntropy
+from cleverhans.model import Model
+from cleverhans.train import train
+from cleverhans.utils import TemporaryLogLevel, set_log_level
+from cleverhans.utils import to_categorical
+from cleverhans.utils_keras import KerasModelWrapper
+from cleverhans.utils_tf import model_eval
+from cleverhans_tutorials.tutorial_models import ModelBasicCNN
+from dataset import MNIST
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import cm
+from matplotlib import pyplot as plt
+from six.moves import xrange
+from tensorflow.python.platform import flags
 
 from image_utils import create_image_folder, ensure_is_plottable
 
@@ -198,3 +219,172 @@ def attackJSM(model, datagen, source_samples=3, save_image_location='atkimageJSM
     plt.close(figure)
     cleverhans.utils.grid_visual(grid_viz_data)
 
+
+def back_to_black(model, dataset, batch_size=128, learning_rate=.001,
+                  holdout=128, data_aug=6,
+                  nb_epochs_s=10, lmbda=.1,
+                  aug_batch_size=512):
+
+    """
+    You've been...thunderstruck
+
+    Note: holdout needs to be a multiple of batch_size lol
+    """
+
+    set_log_level(logging.DEBUG)
+
+    # Dictionary used to keep track and return key accuracies
+    accuracies = {}
+
+    # Create TF session
+    sess = tf.Session()
+
+    x_train, y_train = dataset.train_images, dataset.train_labels
+    x_test, y_test = dataset.test_images, dataset.test_labels
+
+    # Initialize substitute training set reserved for adversary
+    x_sub = x_test[:holdout]
+    y_sub = y_test[:holdout]
+    y_sub_1D = np.argmax(y_sub, axis=1)
+
+    # Redefine test set as remaining samples unavailable to adversaries
+    x_test = x_test[holdout:]
+    y_test = y_test[holdout:]
+
+    # Obtain Image parameters
+    img_rows, img_cols, nchannels = x_train.shape[1:4]
+    nb_classes = y_train.shape[1]
+
+    # Define input TF placeholder
+    x = tf.placeholder(tf.float32, shape=(None, img_rows, img_cols,
+                                          nchannels))
+    y = tf.placeholder(tf.float32, shape=(None, nb_classes))
+
+    # Seed random number generator so tutorial is reproducible
+    rng = np.random.RandomState([2018, 11, 23])
+
+    bbox_preds = get_predictions_as_iterator(batch_size, model, x_train)
+
+    # Test the accuracy on legitimate data
+    accuracies['bbox_acc_legit'] = model.evaluate(x_sub, y_sub)
+
+
+    # Train substitute using method from https://arxiv.org/abs/1602.02697
+    print("Training the substitute model.")
+    train_sub_out = train_sub(sess, x, y, bbox_preds, x_sub, y_sub_1D,
+                              nb_classes, nb_epochs_s, batch_size,
+                              learning_rate, data_aug, lmbda, aug_batch_size,
+                              rng, img_rows, img_cols, nchannels)
+    model_sub, preds_sub = train_sub_out
+
+    # Evaluate the substitute model on clean test examples
+    eval_params = {'batch_size': batch_size}
+    acc = model_eval(sess, x, y, preds_sub, x_test, y_test, args=eval_params)
+    accuracies['sub_acc_legit'] = acc
+
+    # Initialize the Fast Gradient Sign Method (FGSM) attack object.
+    fgsm_par = {'eps': 0.3, 'ord': np.inf, 'clip_min': 0., 'clip_max': 1.}
+    fgsm = FastGradientMethod(model_sub, sess=sess)
+
+    # Craft adversarial examples using the substitute
+    x_adv_sub_array = fgsm.generate_np(x_sub, **fgsm_par)
+    x_adv_sub_tensor = fgsm.generate(x, **fgsm_par)
+
+    # Evaluate the substitute model on the adversarial test examples
+    acc = model_eval(sess, x, y, model_sub.get_logits(x_adv_sub_tensor), x_test, y_test, args=eval_params)
+    accuracies['sub_acc_adv'] = acc
+
+    accuracy = model.evaluate(x_adv_sub_array, y_sub)
+    accuracies['bbox_acc_adv'] = accuracy
+
+    return accuracies
+
+
+def get_predictions_as_iterator(batch_size, model, x_train):
+    preds_tensor = tf.convert_to_tensor(model.predict(x_train))
+    return tf.data.Dataset.from_tensor_slices(preds_tensor).batch(batch_size).make_one_shot_iterator().get_next()
+
+
+class ModelSubstitute(Model):
+    def __init__(self, scope, nb_classes, nb_filters=200, **kwargs):
+        del kwargs
+        Model.__init__(self, scope, nb_classes, locals())
+        self.nb_filters = nb_filters
+
+    def fprop(self, x, **kwargs):
+        del kwargs
+        my_dense = functools.partial(
+            tf.layers.dense, kernel_initializer=HeReLuNormalInitializer)
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+            y = tf.layers.flatten(x)
+            y = my_dense(y, self.nb_filters, activation=tf.nn.relu)
+            y = my_dense(y, self.nb_filters, activation=tf.nn.relu)
+            logits = my_dense(y, self.nb_classes)
+            return {self.O_LOGITS: logits,
+                    self.O_PROBS: tf.nn.softmax(logits=logits)}
+
+
+def train_sub(sess, x, y, bbox_preds, x_sub, y_sub, nb_classes,
+              nb_epochs_s, batch_size, learning_rate, data_aug, lmbda,
+              aug_batch_size, rng, img_rows=28, img_cols=28,
+              nchannels=1):
+    """
+    This function creates the substitute by alternatively
+    augmenting the training data and training the substitute.
+    :param sess: TF session
+    :param x: input TF placeholder
+    :param y: output TF placeholder
+    :param bbox_preds: output of black-box model predictions
+    :param x_sub: initial substitute training data
+    :param y_sub: initial substitute training labels
+    :param nb_classes: number of output classes
+    :param nb_epochs_s: number of epochs to train substitute model
+    :param batch_size: size of training batches
+    :param learning_rate: learning rate for training
+    :param data_aug: number of times substitute training data is augmented
+    :param lmbda: lambda from arxiv.org/abs/1602.02697
+    :param rng: numpy.random.RandomState instance
+    :return:
+    """
+    # Define TF model graph (for the black-box model)
+    model_sub = ModelSubstitute('model_s', nb_classes)
+    preds_sub = model_sub.get_logits(x)
+    loss_sub = CrossEntropy(model_sub, smoothing=0)
+
+    print("Defined TensorFlow model graph for the substitute.")
+
+    # Define the Jacobian symbolically using TensorFlow
+    grads = jacobian_graph(preds_sub, x, nb_classes)
+
+    # Train the substitute and augment dataset alternatively
+    for rho in xrange(data_aug):
+        print("Substitute training epoch #" + str(rho))
+        train_params = {
+            'nb_epochs': nb_epochs_s,
+            'batch_size': batch_size,
+            'learning_rate': learning_rate
+        }
+        with TemporaryLogLevel(logging.WARNING, "cleverhans.utils.tf"):
+            train(sess, loss_sub, x_sub, to_categorical(y_sub, nb_classes),
+                  init_all=False, args=train_params, rng=rng,
+                  var_list=model_sub.get_params())
+
+        # If we are not at last substitute training iteration, augment dataset
+        if rho < data_aug - 1:
+            print("Augmenting substitute training data.")
+            # Perform the Jacobian augmentation
+            lmbda_coef = 2 * int(int(rho / 3) != 0) - 1
+            x_sub = jacobian_augmentation(sess, x, x_sub, y_sub, grads,
+                                          lmbda_coef * lmbda, aug_batch_size)
+
+            print("Labeling substitute training data.")
+            # Label the newly generated synthetic points using the black-box
+            y_sub = np.hstack([y_sub, y_sub])
+            x_sub_prev = x_sub[int(len(x_sub)/2):]
+            bbox_val = batch_eval(sess, [x], [bbox_preds], [x_sub_prev], batch_size=batch_size)[0]
+            # Note here that we take the argmax because the adversary
+            # only has access to the label (not the probabilities) output
+            # by the black-box model
+            y_sub[int(len(x_sub)/2):] = np.argmax(bbox_val, axis=1)
+
+    return model_sub, preds_sub
--- src/dragonfist/ensemble_example.py
+++ src/dragonfist/ensemble_example.py
@@ -14,14 +14,14 @@ def main(train):
 
     dataset = DataSet.load_from_keras(keras.datasets.fashion_mnist)
 
-    claw1 = Claw(dataset, auto_train=True, retrain=train, epochs=1)
-    # test_claw(claw1, dataset)
+    claw1 = Claw(dataset, auto_train=True, retrain=False, epochs=3)
+    test_claw(claw1, dataset)
 
-    claw2 = Claw(dataset, ImageProcessParams(filters.gaussian, {'sigma': 0.5}), auto_train=True, retrain=train, epochs=1)
-    # test_claw(claw2, dataset)
+    claw2 = Claw(dataset, ImageProcessParams(filters.gaussian, {'sigma': 1.0}), auto_train=True, retrain=train, epochs=3)
+    test_claw(claw2, dataset)
 
-    claw3 = Claw(dataset, ImageProcessParams(filters.sobel, {}, tr.compat2d, {'zca_whitening': True}), auto_train=True, retrain=train, epochs=1)
-    # test_claw(claw3, dataset)
+    claw3 = Claw(dataset, ImageProcessParams(filters.sobel, {}, tr.compat2d, {'zca_whitening': True}), auto_train=True, retrain=train, epochs=3)
+    test_claw(claw3, dataset)
 
     ensemble = Stacking(claw1, claw2, claw3)
 
@@ -29,11 +29,12 @@ def main(train):
     x_test = dataset.test_images
     y = dataset.train_labels
     y_test = dataset.test_labels
-    ensemble.fit(x=x, y=y)
 
-    ensemble.evaluate(x_test, y_test)
-    p = ensemble.predict(x[:1]).argmax()
-    print("prediction for image 0: " + str(p))
+    val_acc = ensemble.fit(x=x, y=y)
+    print("Ensemble fit validation accuracy: " + str(val_acc))
+
+    test_acc = ensemble.evaluate(x_test, y_test)
+    print("Ensemble test set accuracy: " + str(test_acc))
 
 
 def test_claw(claw, d):
@@ -41,4 +42,4 @@ def test_claw(claw, d):
     print('Test accuracy: {:.2f}%'.format(test_acc*100))
 
 
-main(False)
+main(True)
